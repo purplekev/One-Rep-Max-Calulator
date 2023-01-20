3ee79b3aa74cde91eb32b2ab35b581b03cf2fa9e
purplekev <101190391+purplekev@users.noreply.github.com>
2023-01-20 15:33:35 +1100

    added functionality to calculator

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function calculateRM() {
+    var weight = document.RMForm.EnterWeight.value;
+    var reps = document.RMForm.NumberofReps.value;
+    var OneRM = +weight + (weight * reps * 0.0333);
+    document.RMForm.RM.value = OneRM;
+}
