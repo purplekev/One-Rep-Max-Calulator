bc5a9249917ba0fc226a4f8e1066ef0857d892f7
purplekev <101190391+purplekev@users.noreply.github.com>
2023-01-20 18:18:23 +1100

    updating files

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ function calculateRM() {
     var weight = document.RMForm.EnterWeight.value;
     var reps = document.RMForm.NumberofReps.value;
     var OneRM = +weight + (weight * reps * 0.0333);
-    document.RMForm.RM.value = OneRM;
+    document.RMForm.RM.value = OneRM.toFixed(2);
 }
