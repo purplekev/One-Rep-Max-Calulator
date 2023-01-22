a3bd0887fcd43486300373148bc4255f2566e3cb
purplekev <101190391+purplekev@users.noreply.github.com>
2023-01-22 19:27:56 +1100

    fixed bugs

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 function calculateRM() {
     var weight = document.RMForm.EnterWeight.value;
     var reps = document.RMForm.NumberofReps.value;
-    var OneRM = +weight + (weight * reps * 0.0333);
-    document.RMForm.RM.value = OneRM.toFixed(2);
+
+    if (reps == 0) {    
+        var OneRM = 0;
+        document.RMForm.RM.value = OneRM.toFixed(2);
+    } else if (reps == 1) {
+        OneRM = weight;
+        document.RMForm.RM.value = OneRM + '.00';
+    } else {
+        var OneRM = +weight + (weight * reps * 0.0333);
+        document.RMForm.RM.value = OneRM.toFixed(2);
+    }
 }
